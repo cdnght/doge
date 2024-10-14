c44d7976b3efedd0469828f75b4551f1bc8f529d
𝖓𝖔𝖝𝖟𝖞 │ شركة <cdnght@outlook.com>
2024-10-14 21:53:36 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.8/SRBMiner-Multi-2-6-8-Linux.tar.gz &&
+tar -xzvf SRBMiner-Multi-2-6-8-Linux.tar.gz &&
+cd SRBMiner-Multi-2-6-8 &&
+agent-stop &&
+cp SRBMiner-MULTI /opt/mmp/miners/sbrminer/ &&
+agent-start &&
+cd SRBMiner-Multi-2-6-8 &&
+./guided-setup.sh &&
+NoxzyV1 &&
+n &&
+minotaurx &&
+stratum+tcp://minotaurx.sea.mine.zpool.ca:7019 &&
+DBbAv4ZWFhjFLGwNo9FgxZPf7HJJWwuLWQ &&
+c=DOGE &&
+n &&
+./start_NoxzyV1.sh
